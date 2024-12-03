cb1163e60e1fd5c17e01063c21bdea8e3665a038
Antonio Benitez <mrsasuu@gmail.com>
2024-12-03 18:03:31 +0100

    Setup Android

--- android/app/src/main/java/com/weatheronboarding/MainActivity.kt
+++ android/app/src/main/java/com/weatheronboarding/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.weatheronboarding
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -12,6 +13,16 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "WeatherOnboarding"
+
+    /**
+     * This method is overridden due to React Navigation integration.
+     * Calling super.onCreate(null) ensures proper initialization
+     * for navigation functionality in the app.
+     * Check: https://reactnavigation.org/docs/getting-started/
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(null)
+    }
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
