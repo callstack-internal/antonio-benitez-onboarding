0402a09a8c34bef884d95eff6553e6fd881e8ef8
Antonio Benitez <mrsasuu@gmail.com>
2024-12-04 09:57:54 +0100

    Add navigator types

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,9 +1,11 @@
+import type {RootStackParamList} from './RootNavigatorParamList';
+
 import {createStaticNavigation} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import LocationListScreen from '../screens/locationList/LocationListScreen';
 
-const RootStack = createNativeStackNavigator({
+const RootStack = createNativeStackNavigator<RootStackParamList>({
   initialRouteName: 'LocationList',
   screens: {
     LocationList: LocationListScreen,
