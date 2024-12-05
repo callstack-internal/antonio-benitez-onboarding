3d4ffec50c89d113edfef88250cbc66cb0d3d3dc
Antonio Benitez <mrsasuu@gmail.com>
2024-12-05 17:19:44 +0100

    Setup MSW and add it to LocationListScreen.tests

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+const enableMocking = async () => {
+  if (!__DEV__) {
+    return;
+  }
+
+  try {
+    console.log('Enabling mocking...');
+
+    await import('./msw.polyfills');
+    console.log('Polyfills loaded.');
+
+    const {server} = await import('./src/mocks/server');
+    console.log('Server imported');
+
+    server.listen();
+
+    console.log('Mocking enabled.');
+  } catch (error) {
+    console.error('Failed to enable mocking:', error);
+  }
+};
+
+enableMocking();
 
 AppRegistry.registerComponent(appName, () => App);
