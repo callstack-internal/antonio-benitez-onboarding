4e58047e4ca7b1902cd94bc56cc53d8b2aff75cf
Antonio Benitez <mrsasuu@gmail.com>
2024-12-06 15:15:19 +0100

    Add DeviceLocationModule iOS implementation

--- device-location-package/DeviceLocationPackage.podspec
+++ device-location-package/DeviceLocationPackage.podspec
@@ -1,34 +1,38 @@
 require "json"
 
 package = JSON.parse(File.read(File.join(__dir__, "package.json")))
-
-new_arch_enabled = ENV['RCT_NEW_ARCH_ENABLED'] == '1'
+folly_compiler_flags = '-DFOLLY_NO_CONFIG -DFOLLY_MOBILE=1 -DFOLLY_USE_LIBCPP=1 -Wno-comma -Wno-shorten-64-to-32'
 
 Pod::Spec.new do |s|
-  s.name            = "DeviceLocationPackage"
-  s.version         = package["version"]
-  s.summary         = package["description"]
-  s.description     = package["description"]
-  s.homepage        = package["homepage"]
-  s.license         = package["license"]
-  s.platforms       = { :ios => "13.0" }
-  s.author          = package["author"]
-  s.source          = { :git => package["repository"], :tag => "#{s.version}" }
+  s.name         = "DeviceLocationPackage"
+  s.version      = package["version"]
+  s.summary      = package["description"]
+  s.homepage     = package["homepage"]
+  s.license      = package["license"]
+  s.authors      = package["author"]
 
-  s.source_files    = "ios/**/*.{h,m,mm,swift}"
+  s.platforms    = { :ios => min_ios_version_supported }
+  s.source       = { :git => ".git", :tag => "#{s.version}" }
 
-  if new_arch_enabled
-    s.pod_target_xcconfig = {
-      "DEFINES_MODULE" => "YES",
-      "SWIFT_OBJC_INTERFACE_HEADER_NAME" => "DeviceLocationPackage-Swift.h",
-      "OTHER_SWIFT_FLAGS" => "-DDEVICE_LOCATION_PACKAGE_NEW_ARCH_ENABLED"
-    }
+  s.source_files = "ios/**/*.{h,m,mm}"
+
+  if respond_to?(:install_modules_dependencies, true)
+    install_modules_dependencies(s)
   else
-    s.pod_target_xcconfig = {
-      "DEFINES_MODULE" => "YES",
-      "SWIFT_OBJC_INTERFACE_HEADER_NAME" => "DeviceLocationPackage-Swift.h"
-    }
-  end
+    s.dependency "React-Core"
 
-  install_modules_dependencies(s)
+    if ENV['RCT_NEW_ARCH_ENABLED'] == '1' then
+      s.compiler_flags = folly_compiler_flags + " -DRCT_NEW_ARCH_ENABLED=1"
+      s.pod_target_xcconfig    = {
+          "HEADER_SEARCH_PATHS" => "\"$(PODS_ROOT)/boost\"",
+          "OTHER_CPLUSPLUSFLAGS" => "-DFOLLY_NO_CONFIG -DFOLLY_MOBILE=1 -DFOLLY_USE_LIBCPP=1",
+          "CLANG_CXX_LANGUAGE_STANDARD" => "c++17"
+      }
+      s.dependency "React-Codegen"
+      s.dependency "RCT-Folly"
+      s.dependency "RCTRequired"
+      s.dependency "RCTTypeSafety"
+      s.dependency "ReactCommon/turbomodule/core"
+    end
+  end
 end
--- device-location-package/ios/DeviceLocationModule.m
+++ device-location-package/ios/DeviceLocationModule.m
@@ -1,58 +1,100 @@
 #import "DeviceLocationModule.h"
-#import "DeviceLocationModuleImpl.h"
 
-@interface DeviceLocationModule () <DeviceLocationModuleImplDelegate>
+@interface DeviceLocationModule() <CLLocationManagerDelegate>
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (copy, nonatomic) RCTPromiseResolveBlock resolve;
+@property (copy, nonatomic) RCTPromiseRejectBlock reject;
+@property (assign, nonatomic) BOOL promiseHandled;
 @end
 
-@implementation DeviceLocationModule {
-    DeviceLocationModuleImpl *_moduleImpl;
-    RCTPromiseResolveBlock _resolveBlock;
-    RCTPromiseRejectBlock _rejectBlock;
-}
+@implementation DeviceLocationModule
 
 RCT_EXPORT_MODULE(DeviceLocationModule)
-
-+ (BOOL)requiresMainQueueSetup {
-    return YES;
-}
-
-- (dispatch_queue_t)methodQueue {
-    return dispatch_get_main_queue();
-}
 
 - (instancetype)init {
     self = [super init];
     if (self) {
-        _moduleImpl = [[DeviceLocationModuleImpl alloc] init];
-        _moduleImpl.delegate = self;
+        self.locationManager = [[CLLocationManager alloc] init];
+        self.locationManager.delegate = self;
+        self.promiseHandled = NO;
     }
     return self;
 }
 
 RCT_EXPORT_METHOD(requestDeviceLocation:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
-    _resolveBlock = resolve;
-    _rejectBlock = reject;
-    [_moduleImpl requestDeviceLocation];
+    self.resolve = resolve;
+    self.reject = reject;
+    self.promiseHandled = NO;
+
+    CLAuthorizationStatus status = self.locationManager.authorizationStatus;
+
+    if (status == kCLAuthorizationStatusNotDetermined) {
+        [self.locationManager requestWhenInUseAuthorization];
+    } else if (status == kCLAuthorizationStatusDenied || status == kCLAuthorizationStatusRestricted) {
+        [self rejectIfNeededWithCode:@"permission_denied" message:@"Location permission denied" error:nil];
+    } else if (status == kCLAuthorizationStatusAuthorizedWhenInUse || status == kCLAuthorizationStatusAuthorizedAlways) {
+        [self.locationManager startUpdatingLocation];
+    } else {
+        [self rejectIfNeededWithCode:@"unknown_status" message:@"Unknown authorization status" error:nil];
+    }
 }
 
-#pragma mark - DeviceLocationModuleImplDelegate
+#pragma mark - CLLocationManagerDelegate
 
-- (void)onLocationSuccess:(CLLocation *)location {
-    NSDictionary *result = @{
-        @"latitude": @(location.coordinate.latitude),
-        @"longitude": @(location.coordinate.longitude),
-        @"accuracy": @(location.horizontalAccuracy)
-    };
-    _resolveBlock(result);
-    _resolveBlock = nil;
-    _rejectBlock = nil;
+- (void)locationManagerDidChangeAuthorization:(CLLocationManager *)manager {
+    if (self.promiseHandled) {
+        return;
+    }
+
+    CLAuthorizationStatus status = manager.authorizationStatus;
+
+    if (status == kCLAuthorizationStatusAuthorizedWhenInUse || status == kCLAuthorizationStatusAuthorizedAlways) {
+        [self.locationManager startUpdatingLocation];
+    } else if (status == kCLAuthorizationStatusDenied || status == kCLAuthorizationStatusRestricted) {
+        [self rejectIfNeededWithCode:@"permission_denied" message:@"Location permission denied" error:nil];
+    }
 }
 
-- (void)onLocationError:(NSError *)error {
-    _rejectBlock(@"LOCATION_ERROR", error.localizedDescription, error);
-    _resolveBlock = nil;
-    _rejectBlock = nil;
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(nonnull NSArray<CLLocation *> *)locations {
+    if (self.promiseHandled) {
+        return;
+    }
+
+    CLLocation *location = [locations lastObject];
+    if (location) {
+        NSDictionary *locationData = @{
+            @"latitude": @(location.coordinate.latitude),
+            @"longitude": @(location.coordinate.longitude),
+            @"accuracy": @(location.horizontalAccuracy)
+        };
+        [self resolveIfNeededWithResult:locationData];
+        [self.locationManager stopUpdatingLocation];
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(nonnull NSError *)error {
+    [self rejectIfNeededWithCode:@"location_error" message:@"Failed to get location" error:error];
+}
+
+#pragma mark - Promise Helpers
+
+- (void)resolveIfNeededWithResult:(id)result {
+    if (!self.promiseHandled && self.resolve) {
+        self.resolve(result);
+        self.promiseHandled = YES;
+        self.resolve = nil;
+        self.reject = nil;
+    }
+}
+
+- (void)rejectIfNeededWithCode:(NSString *)code message:(NSString *)message error:(NSError *)error {
+    if (!self.promiseHandled && self.reject) {
+        self.reject(code, message, error);
+        self.promiseHandled = YES;
+        self.resolve = nil;
+        self.reject = nil;
+    }
 }
 
 @end
